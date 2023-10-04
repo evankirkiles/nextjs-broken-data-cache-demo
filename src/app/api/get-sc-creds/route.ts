e1750a5be0671a5cc8d3620678964ac112660d16
Evan Kirkiles <kirkilese@gmail.com>
2023-10-04 00:31:40 -0400

    Cleaned up variables.

--- src/app/api/get-sc-creds/route.ts
+++ src/app/api/get-sc-creds/route.ts
@@ -1,10 +1,3 @@
-/*
- * route.ts
- * author: evan kirkiles
- * created on Tue Oct 03 2023
- * 2023 the nobot space
- */
-
 import { NextRequest, NextResponse } from "next/server";
 
 declare global {
@@ -20,15 +13,12 @@ export function GET(req: NextRequest) {
     return NextResponse.json("", { status: 401 });
   }
 
-  let SUSPENSE_CACHE_URL: string | null = null;
-  let SUSPENSE_CACHE_ENDPOINT: string | null = null;
-  let SUSPENSE_CACHE_AUTH_TOKEN: string | null = null;
   try {
     const { cacheHandler } = globalThis.__incrementalCache;
     const endpoint = new URL(cacheHandler.cacheEndpoint);
-    SUSPENSE_CACHE_URL = endpoint.hostname;
-    SUSPENSE_CACHE_ENDPOINT = endpoint.pathname.replace("/", "");
-    SUSPENSE_CACHE_AUTH_TOKEN = cacheHandler["headers"][
+    const SUSPENSE_CACHE_URL = endpoint.hostname;
+    const SUSPENSE_CACHE_ENDPOINT = endpoint.pathname.replace("/", "");
+    const SUSPENSE_CACHE_AUTH_TOKEN = cacheHandler["headers"][
       "Authorization"
     ].replace("Bearer ", "");
     return NextResponse.json(
