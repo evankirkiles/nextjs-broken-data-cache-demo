a7b1e57803cd2255d5d7c4cedace3cd9085d68c9
Evan Kirkiles <kirkilese@gmail.com>
2023-10-04 13:37:50 -0400

    Moved get-sc-creds back to its own route handler.

---
+++ src/app/api/get-sc-creds/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+// cache types for intellisense
+import FetchCache from "next/dist/server/lib/incremental-cache/fetch-cache";
+import type { IncrementalCache } from "next/dist/server/lib/incremental-cache";
+
+declare global {
+  var __incrementalCache: IncrementalCache;
+}
+
+export const dynamic = "force-dynamic";
+
+export function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const BUILD_SECRET = searchParams.get("BUILD_SECRET");
+  if (!BUILD_SECRET || BUILD_SECRET !== process.env.BUILD_SECRET) {
+    return NextResponse.json("", { status: 401 });
+  }
+
+  try {
+    const { cacheHandler } = globalThis.__incrementalCache;
+    if (!(cacheHandler instanceof FetchCache)) return NextResponse.json("");
+    const endpoint = new URL(cacheHandler["cacheEndpoint"]);
+    const SUSPENSE_CACHE_URL = endpoint.hostname;
+    const SUSPENSE_CACHE_ENDPOINT = endpoint.pathname.replace("/", "");
+    const SUSPENSE_CACHE_AUTH_TOKEN = cacheHandler["headers"][
+      "Authorization"
+    ].replace("Bearer ", "");
+    return NextResponse.json(
+      `SUSPENSE_CACHE_URL=${SUSPENSE_CACHE_URL} SUSPENSE_CACHE_ENDPOINT=${SUSPENSE_CACHE_ENDPOINT} SUSPENSE_CACHE_AUTH_TOKEN=${SUSPENSE_CACHE_AUTH_TOKEN}`
+    );
+  } catch (e) {}
+  return NextResponse.json("");
+}
