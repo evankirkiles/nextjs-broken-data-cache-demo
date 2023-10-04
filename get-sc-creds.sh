83d4204c5781cc5970013e78f3308c66d538ff62
Evan Kirkiles <kirkilese@gmail.com>
2023-10-04 00:42:19 -0400

    Added minor response validation.

--- get-sc-creds.sh
+++ get-sc-creds.sh
@@ -1,2 +1,6 @@
-get_path="https://$VERCEL_BRANCH_URL/api/get-sc-creds?BUILD_SECRET=$BUILD_SECRET"
-export $(curl -s $get_path | xargs) >/dev/null 2>&1
+get_path="http://$VERCEL_BRANCH_URL/api/get-sc-creds?BUILD_SECRET=$BUILD_SECRET"
+response=$(curl -s $get_path)
+# If we got a valid response, then export to process
+if [[ "$response" == *"SUSPENSE_CACHE_"* ]]; then
+  export $(echo $response | xargs) >/dev/null 2>&1
+fi
