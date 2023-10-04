fae767dd2087548f71d1ee7dc8141c3aff7797ca
Evan Kirkiles <kirkilese@gmail.com>
2023-10-03 21:46:09 -0400

    Added suspense cache auth token.

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,9 +2,6 @@ import { revalidateTag } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
 export function POST(req: NextRequest) {
-  console.log(process.env);
-  console.log(process.env.SUSPENSE_CACHE_AUTH_TOKEN);
-  // console.log(`SUSPENSE_CACHE_URL: ${process.env.SUSPENSE_CACHE_URL}`);
   revalidateTag("time");
   return NextResponse.json({
     success: true,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default async function Home() {
   );
   const data = await currTime.json();
   const output = new Date(data.datetime).toLocaleString();
-  console.log(`SUSPENSE_CACHE_URL: ${process.env.SUSPENSE_CACHE_URL}`);
 
   return (
     <main className={styles.main}>
