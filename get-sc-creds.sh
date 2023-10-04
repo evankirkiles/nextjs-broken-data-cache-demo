61692dbc4c93ddf336d93e53dbc75bf2b74d9fd7
Evan Kirkiles <kirkilese@gmail.com>
2023-10-04 00:10:36 -0400

    Silenced all error output

--- get-sc-creds.sh
+++ get-sc-creds.sh
@@ -1,3 +1,2 @@
 get_path="https://$VERCEL_BRANCH_URL/api/get-sc-creds?BUILD_SECRET=$BUILD_SECRET"
-echo $get_path
-export $(curl -s $get_path | xargs) >/dev/null
+export $(curl -s $get_path | xargs) >/dev/null 2>&1
