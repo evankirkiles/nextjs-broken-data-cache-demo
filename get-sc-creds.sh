49e92cf4d3a67bb390ea8b0fb9c122c22ddd8313
Evan Kirkiles <kirkilese@gmail.com>
2023-10-04 01:34:08 -0400

    Moved data cache access to revalidation route handler.

--- get-sc-creds.sh
+++ get-sc-creds.sh
@@ -1,4 +1,4 @@
-get_path="https://$VERCEL_BRANCH_URL/api/get-sc-creds?BUILD_SECRET=$BUILD_SECRET"
+get_path="https://$VERCEL_BRANCH_URL/api/revalidate?BUILD_SECRET=$BUILD_SECRET"
 response=$(curl -s $get_path)
 # If we got a valid response, then export to process
 if [[ "$response" == *"SUSPENSE_CACHE_"* ]]; then
