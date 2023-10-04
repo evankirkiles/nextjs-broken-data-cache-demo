23a6cdb7eb6b1ee88b96f04feca1b319b47f01e9
Evan Kirkiles <kirkilese@gmail.com>
2023-10-04 00:06:10 -0400

    Replaced vercel url with branch url.

--- get-sc-creds.sh
+++ get-sc-creds.sh
@@ -1,3 +1,3 @@
-get_path="https://$VERCEL_URL/api/get-sc-creds?BUILD_SECRET=$BUILD_SECRET"
+get_path="https://$VERCEL_BRANCH_URL/api/get-sc-creds?BUILD_SECRET=$BUILD_SECRET"
 echo $get_path
 export $(curl -s $get_path | xargs) >/dev/null
